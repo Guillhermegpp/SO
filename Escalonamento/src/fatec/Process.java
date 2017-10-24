90954e9ce1cd4dec536ce35194bd2f86b6639523
Guillhermegpp <guimasax.gp@gmail.com>
2017-10-24 07:39:07 -0300

    alterações

---
+++ Escalonamento/src/fatec/Process.java
@@ -0,0 +1,17 @@
+package fatec;
+
+public class Process implements Comparable<Process> {
+	/*
+	 * ArrayList<Integer> p,burst,tEsp,tResp; public int n=0,j,i, Processo burst
+	 * Tempo de espera Tempo de resposta mEsp=0,Media de espera mResp=0;Media de
+	 * resposta Scanner scan = new Scanner(System.in);
+	 */
+	private int id,nome,burst,tcheg;
+
+	@Override
+	public int compareTo(Process arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
